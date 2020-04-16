9ae4c476ce6a4095aa09c226d8033d83b413f178
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-04-16 15:25:33 +0300

    3d block, last task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,18 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let _min = 99999, _max = -99999;
+  let arrStr = str.split(",");
+  let strWithout_ = arrStr.join(" ");
+  let arrStrLettersNumbers = strWithout_.split(" ");
+  for (let i = 0; i < arrStrLettersNumbers.length; i++) {
+    if (typeof (+arrStrLettersNumbers[i]) == "number"){
+      if (+arrStrLettersNumbers[i] > _max) _max = +arrStrLettersNumbers[i];
+      if (+arrStrLettersNumbers[i] < _min) _min = +arrStrLettersNumbers[i];
+    }
+  }
+  return  {
+    min:_min,
+    max: _max,
+  }
 }
