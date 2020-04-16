7ca02d9853faf795792d05a095a69124989facbf
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-04-16 13:56:27 +0300

    3d block, 3d task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let arrStr = str.split("-");
+  let mappedArrStr = arrStr.map(function (word, index) {
+    if (index != 0 && word != ""){
+      return word[0].toUpperCase() + word.slice(1);
+    }else{
+      return word;
+    }git 
+  });
+  return mappedArrStr.join("");
 }
