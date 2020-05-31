5d5e24a7f771ca31c9fbb05442458815489ad4ca
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-05-31 08:59:10 +0300

    5.2nd task done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  document.addEventListener("click", toggleIt)
+}
+function toggleIt(event) {
+  if (event.target.classList.contains("toggle-text-button")){
+    let text = document.getElementById("text");
+    text.hidden = !text.hidden;
+  }
 }
