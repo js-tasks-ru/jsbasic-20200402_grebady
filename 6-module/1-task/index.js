60d5f99b3d9ff5c635f55b562921ca379c18f41a
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-06-06 12:53:24 +0300

    problem in the task 6.1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,35 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let table = document.createElement("table");
+    let mainRow = document.createElement("tr");
+    let textMainRow = ["Имя", "Возраст", "Зарплата", "Город"];
+    for (let i = 0; i < 4; i++) {
+      let th = document.createElement("th");
+      th.innerHTML = textMainRow[i];
+      mainRow.append(th);
+    }
+    table.append(mainRow);
+    for (let i = 0; i < rows.length; i++) {
+      let row = document.createElement("tr");
+      for (let pieceOfInfo in rows[i]){
+        let th = document.createElement("th");
+        th.innerHTML = rows[i][pieceOfInfo];
+        row.append(th);
+      }
+      let th = document.createElement("th");
+      let deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "X";
+      th.append(deleteButton);
+      row.append(th);
+      table.append(row);
+    }
+    document.addEventListener("click", deleteRow);
+    function deleteRow(event) {
+        if (event.target.closest("button")){
+          event.target.closest("tr").remove();
+        }
+    }
+    this.elem = table;
   }
 }
