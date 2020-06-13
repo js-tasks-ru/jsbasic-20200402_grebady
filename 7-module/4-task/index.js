e3a4d8c9edb1e9d0d9411692cd671f3764ae3b85
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-06-13 08:44:33 +0300

    7th module 4th task done

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,5 +1,8 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+
+    //-----------------<Верстка элемента>
+
     //Корневой элемент
     let slider = document.createElement("div");
     slider.className = "slider";
@@ -29,41 +32,83 @@ export default class StepSlider {
     slider.append(sliderThumb);
     slider.append(sliderSteps);
 
-    //slider.addEventListener("click", chooseSpice);
-    slider.addEventListener("mousedown", chooseSpiceDragAndDrop);
+    //-----------------</Верстка элемента>
+
+    //-----------------Слушаем клик и click и drag-and-drop
+    slider.addEventListener("pointerdown", chooseSpiceDragAndDrop);
+    slider.addEventListener("click", chooseSpiceClick);
 
 
+    //-----------------Общий функционал для отслеживания click'а и drag-and-drop
+    function moveAt(xSliderThumb) {
+      let fullWidthProgress = slider.clientWidth;
+      let leftPercents = Math.round(xSliderThumb/fullWidthProgress * 100);
+      let newValue = 0;
+      let numberOfSegments = steps - 1;
+      if (leftPercents < 0) {
+        sliderThumb.style.left = "0";
+        sliderProgress.style.width = "0";
+        sliderValue.innerText = `${newValue}`;
+      }
+      else if(leftPercents > 100) {
+        sliderThumb.style.left = "100";
+        sliderProgress.style.width = "100";
+        newValue = numberOfSegments;
+        sliderValue.innerText = `${newValue}`;
+      }
+      else {
+        sliderThumb.style.left = `${leftPercents}%`;
+        sliderProgress.style.width = `${leftPercents}%`;
+        newValue = Math.round(xSliderThumb/fullWidthProgress * numberOfSegments);
+        sliderValue.innerText = `${newValue}`;
+      }
+    }
+    function fixLastPos(){
+      let previousChoice = slider.querySelector(".slider__step-active");
+      previousChoice.classList.remove("slider__step-active");
+
+      let fullWidthProgress = slider.clientWidth;
+      let xCenterSliderThumb = event.pageX - slider.offsetLeft;
+      let numberOfSegments = (steps - 1);
+      let fractionToFullWidthProgress = xCenterSliderThumb / fullWidthProgress;
+      let newValue = Math.round(fractionToFullWidthProgress * numberOfSegments);
+      sliderValue.innerText = `${newValue}`;
+      let readyToMakeIntegerSegment = fractionToFullWidthProgress * numberOfSegments;
+      let integerSegment = Math.round(readyToMakeIntegerSegment);
+      let leftPercents = Math.round(integerSegment/numberOfSegments * 100);
+      if (leftPercents < 0) {
+        sliderThumb.style.left = "0";
+        sliderProgress.style.width = "0";
+        newValue = 0;
+      }
+      else if(leftPercents > 100) {
+        sliderThumb.style.left = "100";
+        sliderProgress.style.width = "100";
+        newValue = steps - 1;
+      }
+      else {
+        sliderThumb.style.left = `${leftPercents}%`;
+        sliderProgress.style.width = `${leftPercents}%`;
+      }
+      sliderValue.innerText = `${newValue}`;
+      sliderSteps.children[newValue].classList.add("slider__step-active");
 
 
+      //Событие изменения слайдера
+      let betweenSpanUp = new CustomEvent('slider-change', { // имя события должно быть именно 'slider-change'
+        detail: newValue, // значение 0, 1, 2, 3, 4
+        bubbles: true // событие всплывает - это понадобится в дальнейшем
+      });
 
+      slider.dispatchEvent(betweenSpanUp);
+    }
+    //-----------------Конец общего функционала для отслеживания click'а и drag-and-drop
+
+
+    //-----------------Реализация действий для click и drag-and-drop
     function chooseSpiceDragAndDrop(event){
 
-      function moveAt(xSliderThumb) {
-        let fullWidthProgress = slider.clientWidth;
-        let leftPercents = Math.round(xSliderThumb/fullWidthProgress * 100);
-        let newValue = 0;
-        let numberOfSegments = steps - 1;
-        if (leftPercents < 0) {
-          sliderThumb.style.left = "0";
-          sliderProgress.style.width = "0";
-          sliderValue.innerText = `${newValue}`;
-        }
-        else if(leftPercents > 100) {
-          sliderThumb.style.left = "100";
-          sliderProgress.style.width = "100";
-          newValue = numberOfSegments;
-          sliderValue.innerText = `${newValue}`;
-        }
-        else {
-          sliderThumb.style.left = `${leftPercents}%`;
-          sliderProgress.style.width = `${leftPercents}%`;
-          newValue = Math.round(xSliderThumb/fullWidthProgress * numberOfSegments);
-          sliderValue.innerText = `${newValue}`;
-        }
-
-
-      }
-
+      //-----Набор функций для работы
       function onMouseMove(event) {
         let xCenterSliderThumb = event.pageX - slider.offsetLeft;
         moveAt(xCenterSliderThumb);
@@ -71,59 +116,33 @@ export default class StepSlider {
 
       function onMouseUp(event){
         slider.classList.remove("slider_dragging");
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-        //Событие изменения слайдера
-        let previousChoice = slider.querySelector(".slider__step-active");
-        previousChoice.classList.remove("slider__step-active");
-
-        let fullWidthProgress = slider.clientWidth;
-        let xCenterSliderThumb = event.pageX - slider.offsetLeft;
-        let numberOfSegments = (steps - 1);
-        let fractionToFullWidthProgress = xCenterSliderThumb / fullWidthProgress;
-        let newValue = Math.round(fractionToFullWidthProgress * numberOfSegments);
-        sliderValue.innerText = `${newValue}`;
-        let readyToMakeIntegerSegment = fractionToFullWidthProgress * numberOfSegments;
-        let integerSegment = Math.round(readyToMakeIntegerSegment);
-        let leftPercents = Math.round(integerSegment/numberOfSegments * 100);
-        if (leftPercents < 0) {
-          sliderThumb.style.left = "0";
-          sliderProgress.style.width = "0";
-          newValue = 0;
-        }
-        else if(leftPercents > 100) {
-          sliderThumb.style.left = "100";
-          sliderProgress.style.width = "100";
-          newValue = steps - 1;
-        }
-        else {
-          sliderThumb.style.left = `${leftPercents}%`;
-          sliderProgress.style.width = `${leftPercents}%`;
-        }
-        sliderValue.innerText = `${newValue}`;
-        sliderSteps.children[newValue].classList.add("slider__step-active");
-
-        let betweenSpanUp = new CustomEvent('slider-change', { // имя события должно быть именно 'slider-change'
-          detail: newValue, // значение 0, 1, 2, 3, 4
-          bubbles: true // событие всплывает - это понадобится в дальнейшем
-        });
-
-        slider.dispatchEvent(betweenSpanUp);
+        document.removeEventListener('pointermove', onMouseMove);
+        document.removeEventListener('pointerup', onMouseUp);
+        fixLastPos();
       }
+      //-----Конец набора функций
 
+
+      //-----Логика при drug-and-drop
 
       slider.classList.add("slider_dragging");
       let xSliderThumbStart = event.pageX - slider.offsetLeft;
       moveAt(xSliderThumbStart);
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-
-
+      document.addEventListener('pointermove', onMouseMove);
+      document.addEventListener('pointerup', onMouseUp);
       sliderThumb.ondragstart = function() {
         return false;
       };
     }
+    function chooseSpiceClick(event) {
+      let xSliderThumbStart = event.pageX - slider.offsetLeft;
+      moveAt(xSliderThumbStart);
+      fixLastPos();
+    }
+    //-----------------Конец реализации действий для click и drag-and-drop
 
+
+    //-----------------HTML элемента
     this.elem = slider;
   }
 }
